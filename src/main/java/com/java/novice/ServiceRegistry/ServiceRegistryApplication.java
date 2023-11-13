663d4d0dbeaf6080184d470aaed03361cea00a91
MANJIL DEURI <manjil.d@gmail.com>
2023-11-13 19:57:53 +0530

    Refactor base package

--- src/main/java/com/java/novice/ServiceRegistry/ServiceRegistryApplication.java
+++ src/main/java/com/java/novice/ServiceRegistry/ServiceRegistryApplication.java
@@ -1,4 +1,4 @@
-package com.java.novice;
+package com.java.novice.ServiceRegistry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/java/novice/ServiceRegistry/ServiceRegistryApplicationTests.java
+++ src/test/java/com/java/novice/ServiceRegistry/ServiceRegistryApplicationTests.java
@@ -1,4 +1,4 @@
-package com.java.novice;
+package com.java.novice.ServiceRegistry;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
